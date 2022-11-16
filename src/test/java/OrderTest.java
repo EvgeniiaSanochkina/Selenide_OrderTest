75fa2414b03def99b78d2d901e645ffecbe6f03c
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-11-17 00:20:36 +0700

    OrderTest_edited

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,35 +1,11 @@
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-import java.util.List;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
 public class OrderTest {
-//    private WebDriver driver;
-//    @BeforeAll
-//    static void setupAll() {
-//   WebDriverManager.chromedriver().setup();
-       // System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\IdeaProjects\\task2-2\\chromedriver.exe");
-//    }
-
-//   @BeforeEach
-//    void createBrowser() {
-//        driver = new ChromeDriver();
-//    }
-
     @Test
     void orderTest() {
         open("http://localhost:9999/");
@@ -38,13 +14,5 @@ public class OrderTest {
         $("[data-test-id=\"agreement\"] span").click();
         $(By.className("button")).click();
         $("[data-test-id=\"order-success\"]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-
     }
-
-  //  @AfterEach
-    //void tearDown() {
-      //  driver.quit();
-        //driver = null;
-
-    //}
 }
